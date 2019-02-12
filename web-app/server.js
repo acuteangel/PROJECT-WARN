0970390f1aae8b6bab84b5cab70e388baa8580dd
Kirby <legendofkirby85@gmail.com>
2019-02-11 19:19:48 -0800

    update server for CORS & update npm packages

--- web-app/server.js
+++ web-app/server.js
@@ -5,6 +5,12 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.get('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
