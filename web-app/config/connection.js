c64f873025fb6d47ed82e27f8e04ced4d2954d45
Kirby <legendofkirby85@gmail.com>
2019-02-12 19:48:42 -0800

    update for unity testing

--- web-app/config/connection.js
+++ web-app/config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 		host: 'localhost',
 		user: 'root',
 		password: '',
-		database: 'WARN_leaderboard'
+		database: 'unity_test2'
 	});
 };
 
---
+++ web-app/unity_test/Assets/highscore.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class highscore : MonoBehaviour
+{
+    string highscore_url = "http://localhost:3000";
+    string playName = "Player1";
+    int score = -1;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        // Create a form object for sending high score data to the server
+        WWWForm form = new WWWForm();
+
+        // Assuming the perl script manages high scores for different games
+        form.AddField( "game", "MyGameName" );
+
+        // The name of the player submitting the scores
+        form.AddField( "playerName", playName );
+
+        // The score
+        form.AddField( "score", score );
+
+        // Create a download object
+        var download = UnityWebRequest.Post(highscore_url, form);
+
+        // Wait until the download is done
+        yield return download.SendWebRequest();
+
+        if (download.isNetworkError || download.isHttpError)
+        {
+            print( "Error downloading: " + download.error );
+        }
+        else
+        {
+            // show the highscores
+            Debug.Log(download.downloadHandler.text);
+        }
+    }
+}
